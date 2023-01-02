6706915f2c3ec70ef2da50a69796bd69531d8488
Kotaii <ronaldkwong@RONALDs-MacBook-Air.local>
2023-01-01 21:01:22 -0800

    my first commit

---
+++ pages/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs): #*args, **kwargs
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "home.html", {}) #string of HTML code
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+    my_context={
+        "title": "abc this is about us",
+        "this_is_true":True,
+        "my_number": 123,
+        "my_list": [1313, 4242, 312, "Abc"],
+        "my_html":"<h1>Hello World</h1>"
+    }
+
+    return render(request, "about.html", my_context)
+
+def another_view(request, *args, **kwargs):
+    return render(request, "another.html", {})
+
+
